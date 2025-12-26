5534b7cf8de57ce5711a7c6aa03c952d5108f685
Leonardo <leobarros_@outlook.com>
2025-12-26 20:57:17 -0300

    chore: initial project structure

---
+++ src/test/java/leo/subscription_management_api/SubscriptionManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package leo.subscription_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
