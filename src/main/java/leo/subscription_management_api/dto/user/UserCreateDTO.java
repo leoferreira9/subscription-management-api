40bb53a6ccd8a8f163c592d942e72583c8095ef8
Leonardo <leobarros_@outlook.com>
2025-12-26 22:16:44 -0300

    feat: add User DTOs

---
+++ src/main/java/leo/subscription_management_api/dto/user/UserCreateDTO.java
@@ -0,0 +1,38 @@
+package leo.subscription_management_api.dto.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserCreateDTO {
+
+    @NotBlank
+    @Size(max = 150)
+    private String name;
+
+    @NotBlank
+    @Size(max = 100)
+    private String email;
+
+    public UserCreateDTO() {}
+
+    public UserCreateDTO(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/leo/subscription_management_api/dto/user/UserDTO.java
@@ -0,0 +1,30 @@
+package leo.subscription_management_api.dto.user;
+
+import leo.subscription_management_api.entity.User;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public UserDTO(){};
+
+    private UserDTO(User user){
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
