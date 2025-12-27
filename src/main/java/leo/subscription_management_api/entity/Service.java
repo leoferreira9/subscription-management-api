d21c6718d67057891f9a32c9cca0e71052061d47
Leonardo <leobarros_@outlook.com>
2025-12-26 21:07:19 -0300

    feat: add Service entity

---
+++ src/main/java/leo/subscription_management_api/entity/Service.java
@@ -0,0 +1,34 @@
+package leo.subscription_management_api.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    public Service(){}
+
+    public Service(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
