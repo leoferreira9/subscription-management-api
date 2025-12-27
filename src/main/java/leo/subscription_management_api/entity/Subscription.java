54d2a91294f8dccf162f11c99a98d5549f545201
Leonardo <leobarros_@outlook.com>
2025-12-26 21:31:20 -0300

    feat: add Subscription entity

---
+++ src/main/java/leo/subscription_management_api/entity/Subscription.java
@@ -0,0 +1,134 @@
+package leo.subscription_management_api.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import leo.subscription_management_api.enums.PaymentType;
+import leo.subscription_management_api.enums.SubscriptionStatus;
+import leo.subscription_management_api.enums.SubscriptionType;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "service_id")
+    private Service service;
+
+    @DecimalMin(value = "0.0", inclusive = false)
+    @Column(nullable = false)
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SubscriptionType subscriptionType;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SubscriptionStatus subscriptionStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentType paymentType;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @NotNull
+    @Column(nullable = false)
+    private Date startDate;
+
+    @NotNull
+    @Column(nullable = false)
+    private Date nextBillingDate;
+
+    public Subscription(){}
+
+    public Subscription(Service service, BigDecimal value, SubscriptionType subscriptionType, SubscriptionStatus subscriptionStatus, PaymentType paymentType, User user, Date startDate, Date nextBillingDate) {
+        this.service = service;
+        this.value = value;
+        this.subscriptionType = subscriptionType;
+        this.subscriptionStatus = subscriptionStatus;
+        this.paymentType = paymentType;
+        this.user = user;
+        this.startDate = startDate;
+        this.nextBillingDate = nextBillingDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public SubscriptionType getSubscriptionType() {
+        return subscriptionType;
+    }
+
+    public void setSubscriptionType(SubscriptionType subscriptionType) {
+        this.subscriptionType = subscriptionType;
+    }
+
+    public SubscriptionStatus getSubscriptionStatus() {
+        return subscriptionStatus;
+    }
+
+    public void setSubscriptionStatus(SubscriptionStatus subscriptionStatus) {
+        this.subscriptionStatus = subscriptionStatus;
+    }
+
+    public PaymentType getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(PaymentType paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getNextBillingDate() {
+        return nextBillingDate;
+    }
+
+    public void setNextBillingDate(Date nextBillingDate) {
+        this.nextBillingDate = nextBillingDate;
+    }
+}
