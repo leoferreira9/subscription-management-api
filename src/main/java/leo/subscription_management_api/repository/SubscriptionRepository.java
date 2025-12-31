7f15e4087619f25733dc6de8af15a5a62da75dbf
Leonardo <leobarros_@outlook.com>
2025-12-30 21:01:14 -0300

    fix: correct SubscriptionRepository to use Subscription entity

--- src/main/java/leo/subscription_management_api/repository/SubscriptionRepository.java
+++ src/main/java/leo/subscription_management_api/repository/SubscriptionRepository.java
@@ -1,5 +1,6 @@
 package leo.subscription_management_api.repository;
 
+import leo.subscription_management_api.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubscriptionRepository extends JpaRepository<SubscriptionRepository, Long> {}
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {}
